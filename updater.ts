8e4cc60ddf161b1a551b8d0c2415a8dd31f6abc3
flo <flo>
2020-12-13 16:32:53 +0100

    inc

--- updater.ts
+++ updater.ts
@@ -1,10 +1,14 @@
+const updateUrl = process.env.UPDATE_URL;
+const timeout = process.env.TIMEOUT || 600;
 
-const updateUrl=process.env.UPDATE_URL
-const timeout=process.env.TIMEOUT || 600
-
-if(!updateUrl){
-    console.error("No update URL found")
-    process.abort()
+if (!updateUrl) {
+  console.error("No update URL found.");
+  process.abort();
 }
 
-setTimeout()
+setTimeout(async () => {
+  const res = await fetch(updateUrl);
+  res.status !== 200
+    ? console.error("Dyndns update failed!")
+    : console.log("Dyndns updated successfully.");
+}, timeout);
